a08151aadba5898098c8796d05b87a70a2230e99
root <root@DESKTOP-7RV5LGK.localdomain>
2024-10-11 09:13:37 +0200

    Working veersion v1

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
+import shutil, zipfile, os
+from plan_zajec.serwer import getResponseFor
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"message": "Przejdź do /static/index.html, aby zobaczyć stronę"}
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = f"files/{file.filename}"
+    
+    with open(file_location, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+    print(file_location)
+    getResponseFor("/plan_zajec.zip", file_location)
+
+    with zipfile.ZipFile("static/plan_zajec.zip", 'r') as zip_ref:
+        zip_ref.extractall("static")
