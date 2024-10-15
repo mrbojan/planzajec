84a97399c3a8e50afba0025b3970036b24945477
Bartek <bartlomiej.piotr.bojanowski@gmail.com>
2024-10-15 12:20:43 +0200

    Add download plan.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
 import shutil, zipfile, os
 from plan_zajec.serwer import getResponseFor
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -14,8 +15,16 @@ async def upload_file(file: UploadFile = File(...)):
     with open(file_location, "wb") as f:
         shutil.copyfileobj(file.file, f)
 
-    print(file_location)
     getResponseFor("/plan_zajec.zip", file_location)
 
     with zipfile.ZipFile("static/plan_zajec.zip", 'r') as zip_ref:
         zip_ref.extractall("static")
+    
+    with open('last_file.txt', 'w') as f:
+        f.write(file_location)
+
+@app.get("/plantxt/")
+async def main():
+    with open('last_file.txt', 'r') as f:
+        last_file = f.read()
+    return FileResponse(last_file, media_type='application/octet-stream', filename=last_file.split('/')[-1])
